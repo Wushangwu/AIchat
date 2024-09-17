79d4218f23a85677e434edfdfba2ef484ef2a506
unaG <veraguo11@163.com>
2024-09-17 17:24:12 +0800

    更新安装的版本

--- scripts/install_consul_fabio.sh
+++ scripts/install_consul_fabio.sh
@@ -6,9 +6,9 @@ mkdir /opt/fabio
 export PATH=$PATH:/opt/fabio  #TODO
 cd /opt/fabio
 touch fabio.properties
-wget https://githubfast.com/eBay/fabio/releases/download/v1.3.5/fabio-1.3.5-go1.7.3-linux_amd64
-chmod a+x fabio-1.3.5-go1.7.3-linux_amd64
-ln -s fabio-1.3.5-go1.7.3-linux_amd64 fabio
+wget https://githubfast.com/fabiolb/fabio/releases/download/v1.6.3/fabio-1.6.3-linux_amd64
+chmod a+x fabio-1.6.3-linux_amd64
+ln -s fabio-1.6.3-linux_amd64 fabio
 
 # 启动Consul
 consul agent -dev -ui -node=consul-dev -client=0.0.0.0
